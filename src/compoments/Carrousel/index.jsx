d44f48901bd878022d9801e99d8877a83e2d125a
CedricFournie <cf11157701@gmail.com>
2023-08-23 23:24:32 +0200

    ajout fonction oneelement

--- src/compoments/Carrousel/index.jsx
+++ src/compoments/Carrousel/index.jsx
@@ -8,6 +8,15 @@ function Carrousel(props) {
     const lengarray = logementitem.pictures.length
     const [count, setCount] = useState(1)
 
+    function oneelement () {
+      if (lengarray === 1) {
+        return {
+          display: 'none',
+        };
+      }
+  
+    }
+    
     function sliderleft () {
         setCount(count === 1 ? lengarray : count -1)
     }
@@ -19,9 +28,9 @@ function Carrousel(props) {
     return (
       <div className='divcarrousel'>
         <img className='imgcarrousel' src={logementitem.pictures[count - 1]} alt = "Vue intérieur de l'appartement" />
-        <img className='imgleft' src={arrowleft} alt='Fléche gauche' onClick={() => sliderleft ()} />
-        <img className='imgright' src={arrowright} alt='Fléche droite' onClick={() => slideright ()} />
-        <p className='counter'>{count}/{lengarray}</p>
+        <img className='imgleft' style={oneelement()} src={arrowleft} alt='Fléche gauche' onClick={() => sliderleft ()} />
+        <img className='imgright' style={oneelement()} src={arrowright} alt='Fléche droite' onClick={() => slideright ()} />
+        <p className='counter' style={oneelement()}>{count}/{lengarray}</p>
       </div>
     )
   }
